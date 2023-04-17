da0506b4281be27425da9301694694963deead6b
Ry <OfficiallyRy@outlook.com>
2023-04-16 18:18:35 -0600

    idk man

--- src/AppState.js
+++ src/AppState.js
@@ -1,6 +1,7 @@
 import { reactive } from 'vue'
 import { loadState } from "./utils/Store.js"
 import { Doll } from "./models/Doll.js"
+import { Upgrade } from "./models/Upgrade.js"
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
@@ -12,9 +13,79 @@ export const AppState = reactive({
 
   boosts: [
     {
-
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+      active: false
+    },
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+      active: false
+    },
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+      active: false
+    },
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+      active: false
     },
   ],
+  /** @type {import('./models/Upgrade.js').Upgrade[]} */
+
+  clickUpgrades: [
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+    },
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+    },
+  ],
+
+  autoUpgrades: [
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+    },
+    {
+      name: '',
+      comment: '',
+      description: '',
+      price: 0,
+      value: 0,
+    },
+  ],
+
+  myClickUpgrades: loadState('clickUpgrades', [Upgrade]),
+
+  myAutoUpgrades: loadState('autoUpgrades', [Upgrade]),
+
+  
 
   displaying: 'click',
 })
--- src/models/Upgrade.js
+++ src/models/Upgrade.js
@@ -1,9 +1,10 @@
-export class ClickUpgrade{
+export class Upgrade{
   constructor(data){
     this.name = data.name
+    this.comment = data.comment
     this.description = data.description
     this.price = data.price
     this.value = data.value
-    this.quantity = data.quantity
+    this.quantity = data.quantity || 0
   }
 }
