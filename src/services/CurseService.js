b54b0d2210f40da72f26ab39ba6cd2b1bb932ce3
Ry <OfficiallyRy@outlook.com>
2023-04-24 15:00:11 -0600

    Lowered chances to get essences by a lot teehee

--- src/services/CurseService.js
+++ src/services/CurseService.js
@@ -9,27 +9,27 @@ class CurseService{
     let randomRoll = Math.floor((Math.random() * 1000000) + 1)
     let clickamount = AppState.clickAmount
     AppState.cursePoints += clickamount
-    if(randomRoll < 1000000 && randomRoll > 995000 && AppState.spite == false){
+    if(randomRoll < 1000000 && randomRoll > 999900 && AppState.spite == false){
       AppState.spite = true
       saveState('spite', 'T')
       Pop.toast('You just got a random drop of: Essence of Spite!', 'info', 'top')
     }
-    if(randomRoll < 995000 && randomRoll > 990000 && AppState.judgement == false){
+    if(randomRoll < 999900 && randomRoll > 999800 && AppState.judgement == false){
       AppState.judgement = true
       saveState('judgement', 'T')
       Pop.toast('You just got a random drop of: Essence of Judgement!', 'info', 'top')
     }
-    if(randomRoll < 990000 && randomRoll > 985000 && AppState.manipulation == false){
+    if(randomRoll < 999800 && randomRoll > 999700 && AppState.manipulation == false){
       AppState.manipulation = true
       saveState('manipulation', 'T')
       Pop.toast('You just got a random drop of: Essence of Manipulation!', 'info', 'top')
     }
-    if(randomRoll < 985000 && randomRoll > 980000 && AppState.recklessness == false){
+    if(randomRoll < 999700 && randomRoll > 999600 && AppState.recklessness == false){
       AppState.recklessness = true
       saveState('recklessness', 'T')
       Pop.toast('You just got a random drop of: Essence of Recklessness!', 'info', 'top')
     }
-    if(randomRoll < 980000 & randomRoll > 975000 && AppState.pettiness == false){
+    if(randomRoll < 999600 & randomRoll > 999500 && AppState.pettiness == false){
       AppState.pettiness = true
       saveState('pettiness', 'T')
       Pop.toast('You just got a random drop of: Essence of Pettiness!', 'info', 'top')
