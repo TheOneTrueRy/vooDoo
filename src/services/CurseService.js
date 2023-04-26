fd5888de466f385e8f49ca85f339215639548ad4
Ry <OfficiallyRy@outlook.com>
2023-04-26 03:55:38 -0600

    Added Patchwork dolls

--- src/services/CurseService.js
+++ src/services/CurseService.js
@@ -57,27 +57,27 @@ class CurseService{
     }
     if(randomRoll < 1000000 && randomRoll > 999900 && AppState.spite == false){
       AppState.spite = true
-      saveState('spite', 'T')
+      saveState('spite', true)
       Pop.toast('You just got a random drop of: Essence of Spite!', 'info', 'top')
     }
     if(randomRoll < 999900 && randomRoll > 999800 && AppState.judgement == false){
       AppState.judgement = true
-      saveState('judgement', 'T')
+      saveState('judgement', true)
       Pop.toast('You just got a random drop of: Essence of Judgement!', 'info', 'top')
     }
     if(randomRoll < 999800 && randomRoll > 999700 && AppState.manipulation == false){
       AppState.manipulation = true
-      saveState('manipulation', 'T')
+      saveState('manipulation', true)
       Pop.toast('You just got a random drop of: Essence of Manipulation!', 'info', 'top')
     }
     if(randomRoll < 999700 && randomRoll > 999600 && AppState.recklessness == false){
       AppState.recklessness = true
-      saveState('recklessness', 'T')
+      saveState('recklessness', true)
       Pop.toast('You just got a random drop of: Essence of Recklessness!', 'info', 'top')
     }
     if(randomRoll < 999600 & randomRoll > 999500 && AppState.pettiness == false){
       AppState.pettiness = true
-      saveState('pettiness', 'T')
+      saveState('pettiness', true)
       Pop.toast('You just got a random drop of: Essence of Pettiness!', 'info', 'top')
     }
     saveState('totalStabs', AppState.totalStabs)
