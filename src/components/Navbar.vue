82cb245beb6c8865e025ca18367ed78b6f2deff8
Ry <OfficiallyRy@outlook.com>
2023-04-26 18:16:21 -0600

    BUILD MY SON, BUIIIILLDDDD

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,5 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
     <button
       class="navbar-toggler"
       type="button"
